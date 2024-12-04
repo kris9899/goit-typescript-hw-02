8f3dc48d5d7022957c553d60db080c327bcc520f
Khrystyna Storozhuk <khrystynastorozhuk72@gmail.com>
2024-12-03 21:52:51 -0500

    add new

---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+// export const Card = ({ text }) => {
+//   return <div>{text}</div>;
+// };
+// Оголошення
+export const Card = ({ children }) => {
+  return <div>{children}</div>;
+};
---
+++ src/components/Product.jsx
@@ -0,0 +1,33 @@
+// export default function Product({ name, imgUrl, price }) {
+//   return (
+//     <>
+//       <h2>{name}</h2>
+//       <img src={imgUrl} alt={name} width="480" />
+//       <p>Price: {price} credits</p>
+//     </>
+//   );
+// }
+// src/components/Product.jsx
+
+import PropTypes from 'prop-types';
+
+const Product = ({
+  name,
+  imgUrl = 'https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder',
+  price,
+}) => (
+  <div>
+    <img src={imgUrl} alt={name} width="480" />
+    <h2>{name}</h2>
+    <p>Price: {price} credits</p>
+  </div>
+);
+
+// Опис типів пропсів компонента
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  imgUrl: PropTypes.string,
+  price: PropTypes.number.isRequired,
+};
+
+export default Product;
